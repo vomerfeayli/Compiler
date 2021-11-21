f07d629c89064bbd05a98dbe12b64a1561cdbc85
Ефремов Илья Владимирович <ilya.progromuster@gmail.com>
2021-11-21 19:22:54 +0300

    Небольшие правки и отчёт

--- Compiler/Lexer.cs
+++ Compiler/Lexer.cs
@@ -17,12 +17,15 @@ namespace Compiler
         {
             _code = code;
         }
-
+        
+        /// <summary>
+        /// Возвращаем все токены кода
+        /// </summary>
+        /// <returns></returns>
         public List<Token> LexAnalisis()
         {
-            while (NextToken())
+            while (GetTokensFromCode())
             {
-                //todo хз
 
             }
 
@@ -30,52 +33,56 @@ namespace Compiler
         }
 
         //todo надо красиво переделать
-        private bool NextToken()
+        /// <summary>
+        /// Ищем в коде все токены.
+        /// </summary>
+        /// <returns></returns>
+        private bool GetTokensFromCode()
         {
             if (_code != null && _code.Length == 0)
             {
                 return false;
             }
 
-            for (int i = 0; i < _tokenTypes.Count; i++)
+            foreach (var tokenType in _tokenTypes)
             {
-                var tokenType = _tokenTypes[i];
                 var regex = new Regex("^" + tokenType.Regex);
+                if (_code == null) return true;
                 var result = regex.Match(_code).ToString();
 
-                if (!string.IsNullOrEmpty(result))
+                if (string.IsNullOrEmpty(result)) continue;
+                var _token = new Token { Type = tokenType, LexemName = tokenType.Name, Value = result, Line = _line, Position = _position };
+
+                if (tokenType == _tokenTypes[1] && _tokens.Count != 0 
+                                                && (_tokens[_tokens.Count -1].Type == _tokenTypes[3] || _tokens[_tokens.Count - 1].Type == _tokenTypes[14]))
                 {
-                    var _token = new Token { Type = tokenType, LexemName = tokenType.Name, Value = result, Line = _line, Position = _position };
-
-                    if (tokenType == _tokenTypes[1] && _tokens.Count != 0 
-                        && (_tokens[_tokens.Count -1].Type == _tokenTypes[3] || _tokens[_tokens.Count - 1].Type == _tokenTypes[14]))
-                    {
-                        _token.Type = _tokenTypes[8];
-                        _token.LexemName = _tokenTypes[8].Name;
-                    }
-
-                    if (result != " " && result != "\n")
-                    {
-                        _tokens.Add(_token);
-                    }
-
-                    if (result == "\n")
-                    {
-                        _line++;
-                        _position = 0-1;
-                    }
-
-                    _position += result.Length;
-                    _code =  _code.Substring(result.Length);
-
-                    return true;
+                    _token.Type = _tokenTypes[8];
+                    _token.LexemName = _tokenTypes[8].Name;
                 }
+
+                if (result != " " && result != "\n")
+                {
+                    _tokens.Add(_token);
+                }
+
+                if (result == "\n")
+                {
+                    _line++;
+                    _position = 0-1;
+                }
+
+                _position += result.Length;
+                _code =  _code.Substring(result.Length);
+
+                return true;
             }
 
             throw new CodeException($"Ошибка в коде. Строка {_line}, позиция {_position}");
         }
         
-        
+        /// <summary>
+        /// Список всех токенов.
+        /// </summary>
         private List<TokenType> _tokenTypes = new List<TokenType>
         {
             new TokenType { Name = "Variable", Regex = "Var" },
@@ -92,7 +99,7 @@ namespace Compiler
             new TokenType { Name = "Left bracket", Regex = "\\(" },
             new TokenType { Name = "Right bracket", Regex = "\\)" },
             new TokenType { Name = "Line break", Regex = "\\n" },
-            new TokenType { Name = "Constant", Regex = "[0-9]*" },
+            new TokenType { Name = "Constant", Regex = "[0-9]*" }
         };
     }
 }
--- Compiler/Token.cs
+++ Compiler/Token.cs
@@ -4,11 +4,30 @@ namespace Compiler
 {
     public class Token
     {
+        /// <summary>
+        /// Тип токена.
+        /// </summary>
         [Browsable(false)]
         public TokenType Type { get; set; }
+
+        /// <summary>
+        /// Название лексемы.
+        /// </summary>
         public string LexemName { get; set; }
+
+        /// <summary>
+        /// Значение лексемы.
+        /// </summary>
         public string Value { get; set; }
+
+        /// <summary>
+        /// Номер линии, на которой находиться токен.
+        /// </summary>
         public int Line { get; set; }
+
+        /// <summary>
+        /// Позиция токена на строчке.
+        /// </summary>
         public int Position { get; set; }
     }
 }
